1dc9db18b408fb304feb71824c165ac4501f5c35
Olaf Maas <olaf.maas@bettyblocks.com>
2021-12-01 08:30:02 +0100

    First exercise

--- src/ex1/mod.rs
+++ src/ex1/mod.rs
@@ -1,15 +1,35 @@
 use crate::generic::io::get_lines;
-use crate::generic::parse::split_line;
+use crate::generic::parse::as_int;
 use itertools::Itertools;
 
 pub fn run() {
-    let lines = get_lines("input/ex1/test");
+    let parsed_lines: Vec<i64> = get_lines("input/ex1/ex1")
+        .into_iter()
+        .map(|s| as_int(&s))
+        .collect();
 
-    println!(
-        "{:?}",
-        lines
-            .into_iter()
-            .map(|line| split_line(line, " "))
-            .collect_vec()
-    )
+    part1(parsed_lines.clone());
+    part2(parsed_lines);
+}
+
+fn part1(input: Vec<i64>) {
+    let nr_of_increases = input
+        .into_iter()
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count();
+
+    println!("{:?}", nr_of_increases)
+}
+
+fn part2(input: Vec<i64>) {
+    let nr_of_increases = input
+        .into_iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count();
+
+    println!("{:?}", nr_of_increases)
 }
